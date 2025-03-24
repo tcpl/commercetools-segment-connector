5f483d68469fef3b23db02ee4d637307cfaf87c1
DownChapel <DownChapel@gmail.com>
2025-03-24 17:26:50 +0000

    Log message

--- event-handler/src/app.ts
+++ event-handler/src/app.ts
@@ -1,10 +1,36 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Request, Response } from 'express';
-import { hello } from 'common';
+import { getLogger } from './utils/logger.utils';
+import { decodeToJson } from './utils/decoder.utils';
 
 const app = express();
 app.disable('x-powered-by');
 
-app.post('/deltaSync', (req: Request, res: Response) => {
+app.post('/', async (req: Request, res: Response) => {
+  const logger = getLogger();
+
+  const encodedMessageBody = req.body?.message?.data;
+
+  if (!encodedMessageBody) {
+    logger.info('No message body found in the request!');
+    res.status(204).send();
+    return;
+  }
+
+  const messageBody = decodeToJson(encodedMessageBody);
+  const resourceType = messageBody?.resource?.typeId;
+
+  const notificationType = messageBody.notificationType;
+
+  logger.info('Event message received!');
+  logger.info(`Resource type: ${resourceType}`);
+  logger.info(`Notification type: ${notificationType}`);
+
+  logger.info('Message body:');
+  logger.info(JSON.stringify(messageBody));
+
   res.status(204).send();
 });
 
--- event-handler/src/index.ts
+++ event-handler/src/index.ts
@@ -1,8 +1,9 @@
-import app from "./app";
+import app from './app';
+import { getLogger } from './utils/logger.utils';
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  // const logger = getLogger();
-  console.log(`⚡️ Service listening on port ${PORT}`);
+  const logger = getLogger();
+  logger.info(`⚡️ Service listening on port ${PORT}`);
 });
