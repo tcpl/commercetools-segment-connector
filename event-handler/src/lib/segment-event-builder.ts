74c05ecbc325ca87de91abdec241686ed5a3f634
DownChapel <DownChapel@gmail.com>
2025-03-28 16:51:41 +0000

    Refactor segment-event-builder.ts

--- event-handler/src/lib/segment-event-builder.ts
+++ event-handler/src/lib/segment-event-builder.ts
@@ -16,33 +16,53 @@ export const buildOrderCompletedTrackEvent = (order: Order): TrackParams => {
     throw new Error(`Order ${order.id} is missing taxedShippingPrice`);
   }
 
-  const products = order.lineItems.map((lineItem, i) => {
-    const imageUrl =
-      lineItem.variant.images && lineItem.variant.images.length > 0
-        ? lineItem.variant.images[0]?.url
-        : undefined;
-
-    return {
-      product_id: lineItem.productId,
-      sku: lineItem.variant.sku,
-      price: getTypedMoneyInCurrencyUnits(getLineItemPrice(lineItem)),
-      quantity: lineItem.quantity,
-      image_url: imageUrl,
-      position: i + 1,
-    };
-  });
-
-  const subTotalCentAmount =
+  const subTotalCents =
     order.taxedPrice.totalNet.centAmount -
     order.taxedShippingPrice.totalNet.centAmount;
 
-  const shippingCentAmount = order.taxedShippingPrice.totalGross.centAmount;
+  const shippingTotalCents = order.taxedShippingPrice.totalGross.centAmount;
 
-  const subTotalCurrencyUnits = getCentAmountInCurrencyUnits(
-    subTotalCentAmount,
-    order.totalPrice.fractionDigits
-  );
+  const discountTotalCents = calculateDiscountTotalCents(order);
 
+  // https://segment.com/docs/connections/spec/ecommerce/v2/#order-completed
+
+  return {
+    userId: order.customerId as string, // need either userId or anonymousId
+    anonymousId: order.anonymousId,
+    timestamp: order.createdAt,
+    messageId: `${order.id}-order-completed`,
+    event: 'Order Completed',
+    properties: {
+      email: order.customerEmail,
+      order_id: order.id,
+      total: getTypedMoneyInCurrencyUnits(order.taxedPrice.totalGross),
+      subtotal: getCentAmountInCurrencyUnits(
+        subTotalCents,
+        order.totalPrice.fractionDigits
+      ),
+      discount: getCentAmountInCurrencyUnits(
+        discountTotalCents,
+        order.totalPrice.fractionDigits
+      ),
+      shipping: getCentAmountInCurrencyUnits(
+        shippingTotalCents,
+        order.totalPrice.fractionDigits
+      ),
+      tax:
+        order.taxedPrice.totalTax !== undefined
+          ? getTypedMoneyInCurrencyUnits(order.taxedPrice.totalTax)
+          : undefined,
+      coupon:
+        order.discountCodes && order.discountCodes.length > 0
+          ? order.discountCodes[0].discountCode
+          : undefined,
+      products: buildProducts(order),
+      currency: order.totalPrice.currencyCode,
+    },
+  };
+};
+
+const calculateDiscountTotalCents = (order: Order): number => {
   let discountTotalCents = order.lineItems.reduce((acc, lineItem) => {
     return (
       acc +
@@ -72,39 +92,7 @@ export const buildOrderCompletedTrackEvent = (order: Order): TrackParams => {
 
   discountTotalCents += getShippingDiscountInCents(order);
 
-  // https://segment.com/docs/connections/spec/ecommerce/v2/#order-completed
-
-  return {
-    userId: order.customerId as string, // need either userId or anonymousId
-    anonymousId: order.anonymousId,
-    timestamp: order.createdAt,
-    messageId: `${order.id}-order-completed`,
-    event: 'Order Completed',
-    properties: {
-      email: order.customerEmail,
-      order_id: order.id,
-      total: getTypedMoneyInCurrencyUnits(order.taxedPrice.totalGross), // Subtotal ($) with shipping and taxes added in
-      subtotal: subTotalCurrencyUnits, // subtotal: Order total after discounts but before taxes and shipping
-      discount: getCentAmountInCurrencyUnits(
-        discountTotalCents,
-        order.totalPrice.fractionDigits
-      ),
-      shipping: getCentAmountInCurrencyUnits(
-        shippingCentAmount,
-        order.totalPrice.fractionDigits
-      ),
-      tax:
-        order.taxedPrice.totalTax !== undefined
-          ? getTypedMoneyInCurrencyUnits(order.taxedPrice.totalTax)
-          : undefined,
-      coupon:
-        order.discountCodes && order.discountCodes.length > 0
-          ? order.discountCodes[0].discountCode
-          : undefined,
-      products,
-      currency: order.totalPrice.currencyCode,
-    },
-  };
+  return discountTotalCents;
 };
 
 const getShippingDiscountInCents = (order: Order) => {
@@ -126,12 +114,6 @@ const getShippingDiscountInCents = (order: Order) => {
   return getShippingInfoDiscountInCents(order.shippingInfo);
 };
 
-const getLineItemPrice = (lineItem: LineItem) => {
-  return lineItem.price.discounted
-    ? lineItem.price.discounted.value
-    : lineItem.price.value;
-};
-
 const getShippingInfoDiscountInCents = (shippingInfo: ShippingInfo) => {
   const discountedCentAmount = shippingInfo.discountedPrice?.value?.centAmount;
 
@@ -140,6 +122,12 @@ const getShippingInfoDiscountInCents = (shippingInfo: ShippingInfo) => {
   }
 
   return shippingInfo.price.centAmount - discountedCentAmount;
+};
+
+const getLineItemPrice = (lineItem: LineItem) => {
+  return lineItem.price.discounted
+    ? lineItem.price.discounted.value
+    : lineItem.price.value;
 };
 
 const getTypedMoneyInCurrencyUnits = (money: TypedMoney) => {
@@ -153,4 +141,22 @@ const getCentAmountInCurrencyUnits = (
   return new Decimal(centAmount)
     .div(new Decimal(10).pow(fractionDigits))
     .toNumber();
+};
+
+const buildProducts = (order: Order) => {
+  return order.lineItems.map((lineItem, i) => {
+    const imageUrl =
+      lineItem.variant.images && lineItem.variant.images.length > 0
+        ? lineItem.variant.images[0]?.url
+        : undefined;
+
+    return {
+      product_id: lineItem.productId,
+      sku: lineItem.variant.sku,
+      price: getTypedMoneyInCurrencyUnits(getLineItemPrice(lineItem)),
+      quantity: lineItem.quantity,
+      image_url: imageUrl,
+      position: i + 1,
+    };
+  });
 };
