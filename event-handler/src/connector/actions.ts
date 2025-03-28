c4afadd5194584ac65915ea054334e9834244e7f
DownChapel <DownChapel@gmail.com>
2025-03-28 17:06:16 +0000

    Revert "Temp commit"
    This reverts commit 93cb98483c33416400d6e4ba792612c44061ef49.

--- event-handler/src/connector/actions.ts
+++ event-handler/src/connector/actions.ts
@@ -3,7 +3,7 @@ import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/dec
 const SUBSCRIPTION_KEY = 'tcpl-segment-subscription';
 
 export async function createSubscription(
-  apiRoot: any,
+  apiRoot: ByProjectKeyRequestBuilder,
   topicName: string,
   projectId: string
 ) {
