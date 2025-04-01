0f2a59b54bbe05359198257f28741250d9b38708
DownChapel <DownChapel@gmail.com>
2025-04-01 16:46:00 +0100

    Initial work on high level tests

---
+++ event-handler/src/app.test.ts
@@ -0,0 +1,116 @@
+import request from 'supertest';
+import app from './app';
+import { createApiRoot } from './client/create.client';
+// import { Analytics } from '@segment/analytics-node';
+
+jest.mock('./client/create.client');
+jest.mock('@segment/analytics-node');
+
+// const mockTrack = jest.fn();
+
+beforeEach(() => {
+  jest.clearAllMocks();
+
+  // (Analytics as jest.Mock).mockImplementation(() => ({
+  //   track: mockTrack,
+  // }));
+});
+
+it('should handle customer ResourceCreated event', async () => {
+  const mockGetCustomer = jest.fn().mockResolvedValue({
+    body: { id: 'customer-id-123', email: 'test@example.com' },
+  });
+  (createApiRoot as jest.Mock).mockReturnValue({
+    customers: () => ({
+      withId: () => ({ get: () => ({ execute: mockGetCustomer }) }),
+    }),
+  });
+
+  await request(app)
+    .post('/')
+    .send({
+      message: {
+        data: Buffer.from(
+          JSON.stringify({
+            resource: { typeId: 'customer', id: 'customer-id-123' },
+            notificationType: 'ResourceCreated',
+          })
+        ).toString('base64'),
+      },
+    })
+    .expect(204);
+
+  expect(mockGetCustomer).toHaveBeenCalled();
+});
+
+// it('should handle order ResourceCreated event', async () => {
+//   const mockGetOrder = jest.fn().mockResolvedValue({
+//     body: { id: 'order-id-789', customerEmail: 'order@example.com' },
+//   });
+//   const mockIdentify = jest.fn();
+//   const mockTrack = jest.fn();
+
+//   (createApiRoot as jest.Mock).mockReturnValue({
+//     orders: () => ({
+//       withId: () => ({ get: () => ({ execute: mockGetOrder }) }),
+//     }),
+//   });
+
+//   (Analytics as jest.Mock).mockImplementation(() => ({
+//     identify: mockIdentify,
+//     track: mockTrack,
+//   }));
+
+//   await request(app)
+//     .post('/')
+//     .send({
+//       message: {
+//         data: Buffer.from(
+//           JSON.stringify({
+//             resource: { typeId: 'order', id: 'order-id-789' },
+//             notificationType: 'ResourceCreated',
+//           })
+//         ).toString('base64'),
+//       },
+//     })
+//     .expect(204);
+
+//   expect(mockGetOrder).toHaveBeenCalled();
+//   expect(mockTrack).toHaveBeenCalled();
+// });
+
+// it('should ignore unsupported resource types', async () => {
+//   await request(app)
+//     .post('/')
+//     .send({
+//       message: {
+//         data: Buffer.from(
+//           JSON.stringify({
+//             resource: { typeId: 'unsupported', id: 'resource-id' },
+//             notificationType: 'ResourceCreated',
+//           })
+//         ).toString('base64'),
+//       },
+//     })
+//     .expect(204);
+
+//   expect(createApiRoot).not.toHaveBeenCalled();
+// });
+
+// it('should ignore unsupported notification types', async () => {
+//   await request(app)
+//     .post('/')
+//     .send({
+//       message: {
+//         data: Buffer.from(
+//           JSON.stringify({
+//             resource: { typeId: 'customer', id: 'customer-id-123' },
+//             notificationType: 'UnsupportedType',
+//           })
+//         ).toString('base64'),
+//       },
+//     })
+//     .expect(204);
+
+//   expect(createApiRoot).not.toHaveBeenCalled();
+// });
