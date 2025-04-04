865f7238721db1845e688bb54b808c56385e5590
DownChapel <DownChapel@gmail.com>
2025-04-04 09:55:42 +0100

    Rename error class

--- event-handler/src/validators/message.validators.ts
+++ event-handler/src/validators/message.validators.ts
@@ -1,26 +1,26 @@
 import { Request } from 'express';
-import CustomError from '../errors/custom.error';
+import ServiceError from '../errors/service.error';
 import { decodeToJson } from '../utils/decoder.utils';
 
 const HTTP_STATUS_BAD_REQUEST = 400;
 
 export async function validateMessageBody(request: Request) {
   if (!request.body) {
-    throw new CustomError(
+    throw new ServiceError(
       HTTP_STATUS_BAD_REQUEST,
       'Bad request: No Pub/Sub message was received'
     );
   }
 
   if (!request.body.message) {
-    throw new CustomError(
+    throw new ServiceError(
       HTTP_STATUS_BAD_REQUEST,
       'Bad request: Wrong No Pub/Sub message format - Missing body message'
     );
   }
 
   if (!request.body.message.data) {
-    throw new CustomError(
+    throw new ServiceError(
       HTTP_STATUS_BAD_REQUEST,
       'Bad request: Wrong No Pub/Sub message format - Missing data in body message'
     );
