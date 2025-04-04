91d204e1fdf7ba1dfa339d2aac85be5417d02d9a
DownChapel <DownChapel@gmail.com>
2025-04-04 14:02:35 +0100

    Graceful shutdown

--- event-handler/src/index.ts
+++ event-handler/src/index.ts
@@ -1,9 +1,20 @@
 import app from './app';
+import { getAnalytics } from './lib/segment-analytics-client-factory';
 import { getLogger } from './utils/logger.utils';
 
 const PORT = 8080;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   const logger = getLogger();
   logger.info(`⚡️ Service listening on port ${PORT}`);
 });
+
+const onExit = async () => {
+  await getAnalytics().flush({ close: true });
+
+  server.close(() => {
+    process.exit();
+  });
+};
+
+['SIGINT', 'SIGTERM'].forEach((code) => process.on(code, onExit));
