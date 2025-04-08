8336a31d37278fe9950386d3bf33436b45bbdbde
DownChapel <DownChapel@gmail.com>
2025-04-08 09:27:25 +0100

    Revert "Test invalid customer error"
    This reverts commit 32c515943221991f6e4f2eb61bb42238da6a5060.

--- event-handler/src/lib/customer-event-handler.ts
+++ event-handler/src/lib/customer-event-handler.ts
@@ -4,7 +4,6 @@ import { deleteUser } from './segment-user-deletion-service';
 
 export async function handleCustomerUpsert(customerId: string) {
   const customer = await getCustomer(customerId);
-  await getCustomer('2301477e-c2a6-497c-ae66-62bd418ea4f8');
 
   await identifyCustomer(customer);
 }
