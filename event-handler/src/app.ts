9127f014f8c43d10ea27f37c783c70db1718429e
DownChapel <DownChapel@gmail.com>
2025-04-08 08:09:42 +0100

    Handle ServiceError

--- event-handler/src/app.ts
+++ event-handler/src/app.ts
@@ -9,37 +9,50 @@ import {
 } from './lib/customer-event-handler';
 import { handleOrderCreated } from './lib/order-event-handler';
 import { validateMessageBody } from './validators/message.validators';
+import ServiceError from './errors/service.error';
+import { getLogger } from './utils/logger.utils';
 
 const app = express();
 app.disable('x-powered-by');
 app.use(bodyParser.json());
 
 app.post('/', async (req: Request, res: Response) => {
-  const messageBody = await validateMessageBody(req);
-  const resourceType = messageBody.resource.typeId;
+  const logger = getLogger();
 
-  const notificationType = messageBody.notificationType;
+  try {
+    const messageBody = await validateMessageBody(req);
+    const resourceType = messageBody.resource.typeId;
 
-  const resourceId = messageBody.resource.id;
+    const notificationType = messageBody.notificationType;
 
-  if (resourceType === 'customer') {
-    switch (notificationType) {
-      case 'ResourceCreated':
-      case 'ResourceUpdated':
-        await handleCustomerUpsert(resourceId);
-        break;
-      case 'ResourceDeleted':
-        await handleCustomerDeletion(resourceId);
-        break;
+    const resourceId = messageBody.resource.id;
+
+    if (resourceType === 'customer') {
+      switch (notificationType) {
+        case 'ResourceCreated':
+        case 'ResourceUpdated':
+          await handleCustomerUpsert(resourceId);
+          break;
+        case 'ResourceDeleted':
+          await handleCustomerDeletion(resourceId);
+          break;
+      }
+    } else if (
+      resourceType === 'order' &&
+      notificationType === 'ResourceCreated'
+    ) {
+      await handleOrderCreated(resourceId);
     }
-  } else if (
-    resourceType === 'order' &&
-    notificationType === 'ResourceCreated'
-  ) {
-    await handleOrderCreated(resourceId);
-  }
 
-  res.status(204).send();
+    res.status(204).send();
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      throw error;
+    } else {
+      logger.error('Error processing message', error);
+      res.status(500).send();
+    }
+  }
 });
 
 export default app;
